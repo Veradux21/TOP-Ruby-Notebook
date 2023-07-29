615734d400ec755e77a35aea7d6f6d6aee512d33
matterramun <Veradux4@protonmail.com>
2023-07-29 16:22:23 -0400

    Add Initial Tests. Lint ruby file.

--- Basic Projects/caesar_cipher.rb
+++ Basic Projects/caesar_cipher.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
 
 def caesar_cipher(string, shift_factor)
-  exploded_chars = string.split("")
-  exploded_ords = exploded_chars.map {|char| char.ord}
+  exploded_chars = string.chars
+  exploded_ords = exploded_chars.map(&:ord)
   shifted_ords = []
   exploded_ords.each do |ord|
     case ord
     when 65..90
-      ord = ord + shift_factor
+      ord += shift_factor
       ord -= 25 if ord > 90
-      chr = ord.chr
+      ord.chr
       shifted_ords.push(ord)
     when 97..122
-      ord = ord + shift_factor
+      ord += shift_factor
       ord -= 25 if ord > 122
       chr = ord.chr
       shifted_ords.push(chr)
@@ -20,8 +21,5 @@ def caesar_cipher(string, shift_factor)
       shifted_ords.push(chr)
     end
   end
-  shifted_ords.join("")
+  shifted_ords.join
 end
-
-p caesar_cipher("its a super test", 5)
-p caesar_cipher("nyx f xzujw yjxy", -5)
---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# spec/caesar_cipher_spec.rb
+require './Basic Projects/caesar_cipher'
+
+describe '#caesar_cipher' do
+  it 'rotates the string forward' do
+    expect(caesar_cipher('its a super test', 5)).to eql('nyx f xzujw yjxy')
+  end
+  it 'rotates the string backwards' do
+    expect(caesar_cipher('nyx f xzujw yjxy', -5)).to eql('its a super test')
+  end
+end
