858b4e0ce2f6bbb5ba16eb16d77b93456cd0ad72
matterramun <Veradux4@protonmail.com>
2023-07-29 16:25:28 -0400

    Add exceed ord test.

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -10,4 +10,7 @@ describe '#caesar_cipher' do
   it 'rotates the string backwards' do
     expect(caesar_cipher('nyx f xzujw yjxy', -5)).to eql('its a super test')
   end
+  it 'corrects for exceeding ord range' do
+    expect(caesar_cipher('its a super test', 30)).to eql('nyx f xzujw yjxy')
+  end
 end
